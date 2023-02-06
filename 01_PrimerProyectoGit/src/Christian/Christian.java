4de97d7315171c427c9857c3f2d935cc3375760b
christian.casado <christian.casado@gmail.com>
2023-02-06 18:57:54 +0700

    add mis tres comidas

--- 01_PrimerProyectoGit/src/Christian/Christian.java
+++ 01_PrimerProyectoGit/src/Christian/Christian.java
@@ -5,6 +5,7 @@ public class Christian {
 	public void saludoChristian() {
 		System.out.println("Hola, soy Christian");
 		System.out.println("Mis hobbies son leer, el cine y hacer deporte");
+		System.out.println("Paella, pizza y marisco");
 	}
 
 }
