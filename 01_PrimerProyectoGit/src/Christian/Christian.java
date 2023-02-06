98120b521ebab8e85ef0f567b12bd43ccb9f96e2
christian.casado <christian.casado@gmail.com>
2023-02-06 18:22:55 +0700

    creados los hobbies

--- 01_PrimerProyectoGit/src/Christian/Christian.java
+++ 01_PrimerProyectoGit/src/Christian/Christian.java
@@ -1,8 +1,10 @@
 package Christian;
 
 public class Christian {
+	
 	public void saludoChristian() {
 		System.out.println("Hola, soy Christian");
+		System.out.println("Mis hobbies son leer, el cine y hacer deporte");
 	}
 
 }
