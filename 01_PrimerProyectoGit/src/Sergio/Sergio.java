4669b71419cfe09921aa543a7f45b47c80e6656a
sergio palmero <sergiopalmero87@gmail.com>
2023-02-08 20:21:04 +0100

    añadidad documentación básica

--- 01_PrimerProyectoGit/src/Sergio/Sergio.java
+++ 01_PrimerProyectoGit/src/Sergio/Sergio.java
@@ -1,10 +1,26 @@
 package Sergio;
-
+/**
+ * El objetivo de esta clase es proprocionar informacion sobre nosotros mismos
+ * 
+ * Tendrá diferentes métodos para realizar distintas acciones como ssaludar, saber cuáles son nuestros hobbies y comidas favoritas
+ * @author Sergio Palmero
+ * @version 1.0
+ *
+ */
 public class Sergio {
+	/**
+	 * Método que imprime por pantalla "Hola, soy Sergio"
+	 * 
+	 */
 	public void saludo() {
 		System.out.println("Hola, soy Sergio");
 	}
 	
+	/**
+	 * Método que imprime por pantalla "Deporte, musica, comida"
+	 * También imprime por pantalla "Arroz, garbanzos, lentejas"
+	 *
+	 */
 	public void hobbies() {
 		System.out.println("Deporte, musica, comida");
 		System.out.println("Arroz, garbanzos, lentejas");
