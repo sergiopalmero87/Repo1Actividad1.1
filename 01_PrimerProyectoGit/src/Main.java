8b0aab40fbfe498d5e045ea8c3137bfb956b583f
sergio palmero <sergiopalmero87@gmail.com>
2023-02-08 19:55:09 +0100

    Cambios

--- 01_PrimerProyectoGit/src/Main.java
+++ 01_PrimerProyectoGit/src/Main.java
@@ -1,9 +1,17 @@
+import Christian.Christian;
+import Sergio.Sergio;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Sergio sergio = new Sergio();
+		Christian christian = new Christian();
+		System.out.println("SERGIO");
+		sergio.saludo();
+		sergio.hobbies();
+		System.out.println("CHRISTIAN");
+		christian.saludoChristian();
+		
 	}
 
 }
